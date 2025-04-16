82e0cba551b80d4321cb94ba73d1609237583852
Rab3ie <mohamedmahmoud738@gmail.com>
2025-04-16 18:05:24 +0200

    16-04-2025

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ProjectSidebar from "./component/ProjectSidebar";
+
+
+
+function App() {
+  return (
+    <>
+      <main  className="h-screen my-8 flex gap-8">
+        <ProjectSidebar />
+      </main>
+    </>
+  );
+}
+
+export default App;
