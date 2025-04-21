51d9eb563c516414982a4ea0b3555f047e091952
Mohamed Rabeay <mohamedmahmoud738@gmail.com>
2025-04-21 17:34:47 +0200

    21/04/2025(17:34)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,27 @@ import ProjectSidebar from "./component/ProjectSidebar";
 import NoProjectSelected from "./component/NoProjectSelected";
 import { useState } from "react";
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectProjectId: undefined,
+    projects:[],
+  });
+  let content;
+  if(projectsState.selectProjectId === null){
+    
+    content = <NewProject />
+  }else if(projectsState.selectProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
+
+  function handleStartAddProject(){
+    setProjectsState(prevProject => {
+     return{
+      ...prevProject,
+      selectProjectId: null,
+     };
+    });
+  }
   return (
     <>
       <main  className="h-screen my-8 flex gap-8">
